d1e8c3e018be8dc849e4d386850419c9f402c4ac
Alfonso Gonzalez <agonzalez@demafront.org>
2026-09-22 03:53:20 -0300

    fix: configuracion de cors para que acepte las url de los ms

--- src/main/java/com/rrhh/trabajadores/config/CorsConfig.java
+++ src/main/java/com/rrhh/trabajadores/config/CorsConfig.java
@@ -6,10 +6,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOriginPatterns("http://localhost:3000", "http://*.elb.amazonaws.com")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
